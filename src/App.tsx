26c8035dc5d5e9edbface359bc583207f7b7f539
pengzhen <pengzhen@zhigui.com>
2020-11-11 16:16:03 +0800

    feat:添加共用样式文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import React from 'react';
-import Button, { ButtonType, ButtonSize } from './components/Button/button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button>default</Button>
-        <Button disabled>disabled</Button>
-        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>large</Button>
-        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>small</Button>
-        <Button btnType={ButtonType.Link} size={ButtonSize.Small}>small</Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        test
       </header>
     </div>
   );
